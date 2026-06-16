d667975d26dcf2fe349bce38e943eb9a38de93e2
Alexandre Bouharira-Thelliez <abouharirathelliez@gmail.com>
2026-06-17 01:50:05 +0200

    last version

--- washboard/src/app/(public)/signup/page.tsx
+++ washboard/src/app/(public)/signup/page.tsx
@@ -90,7 +90,7 @@ export default function SignupPage() {
                   value={name}
                   onChange={e => setName(e.target.value)}
                   required
-                  placeholder="Kooki Clean"
+                  placeholder="CleanCar"
                   className={inputClass}
                 />
               </div>
